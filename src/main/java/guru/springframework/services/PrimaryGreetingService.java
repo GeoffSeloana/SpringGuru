1207b9684bbba864937822cabf3a7e6fc56ed6aa
geoff <geoff.seloana@gmail.com>
2018-06-11 22:53:46 +0200

    Added more stuff

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,9 +1,11 @@
 package guru.springframework.services;
 
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
+@Primary
 public class PrimaryGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
